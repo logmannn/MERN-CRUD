d150d47c325c197b6bac69fa34b544195c63b40e
lt <logantanous@gmail.com>
2018-08-27 17:09:10 -0500

    add ability to add and remove objects and update store

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
-import { graphql } from "react-apollo";
+import { graphql, compose } from "react-apollo";
 import Paper from "@material-ui/core/Paper";
 import styled from "styled-components";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const TodosQuery = gql`
   {
@@ -18,7 +25,60 @@ const Flex = styled.div`
   display: flex;
 `;
 
+const UpdateMutation = gql`
+  mutation($id: ID!, $complete: Boolean!) {
+    updateTodo(id: $id, complete: $complete)
+  }
+`;
+
+const RemoveMutation = gql`
+  mutation($id: ID!) {
+    removeTodo(id: $id)
+  }
+`;
+
 class App extends Component {
+  updateTodo = async todo => {
+    await this.props.updateTodo({
+      variables: {
+        id: todo.id,
+        complete: !todo.complete
+      },
+      update: store => {
+        // Read the data from our cache for this query.
+        const data = store.readQuery({ query: TodosQuery });
+        // Add our comment from the mutation to the end.
+        data.todos = data.todos.map(
+          x =>
+            x.id === todo.id
+              ? {
+                  ...todo,
+                  complete: !todo.complete
+                }
+              : x
+        );
+        // Write our data back to the cache.
+        store.writeQuery({ query: TodosQuery, data });
+      }
+    });
+  };
+
+  removeTodo = async todo => {
+    await this.props.removeTodo({
+      variables: {
+        id: todo.id
+      },
+      update: store => {
+        // Read the data from our cache for this query.
+        const data = store.readQuery({ query: TodosQuery });
+        // Add our comment from the mutation to the end.
+        data.todos = data.todos.filter(x => x.id !== todo.id);
+        // Write our data back to the cache.
+        store.writeQuery({ query: TodosQuery, data });
+      }
+    });
+  };
+
   render() {
     const {
       data: { loading, todos }
@@ -30,11 +90,29 @@ class App extends Component {
       <Flex>
         <div style={{ margin: "auto", width: 400 }}>
           <Paper elevation={1}>
-            {todos.map(todo => (
-              <div key={todo.id}>
-                {todo.text} {todo.complete.toString()}
-              </div>
-            ))}
+            <List>
+              {todos.map(todo => (
+                <ListItem
+                  key={todo.id}
+                  role={undefined}
+                  dense
+                  button
+                  onClick={() => this.updateTodo(todo)}
+                >
+                  <Checkbox
+                    checked={todo.complete}
+                    tabIndex={-1}
+                    disableRipple
+                  />
+                  <ListItemText primary={todo.text} />
+                  <ListItemSecondaryAction>
+                    <IconButton onClick={() => this.removeTodo(todo)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
           </Paper>
         </div>
       </Flex>
@@ -42,4 +120,8 @@ class App extends Component {
   }
 }
 
-export default graphql(TodosQuery)(App);
+export default compose(
+  graphql(RemoveMutation, { name: "removeTodo" }),
+  graphql(UpdateMutation, { name: "updateTodo" }),
+  graphql(TodosQuery)
+)(App);
